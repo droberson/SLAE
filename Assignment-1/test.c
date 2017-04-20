63d3aca3bd7cb725297b0d8066f326e9fb8fc255
Daniel Roberson <daniel@planethacker.net>
2017-04-20 05:56:37 -0700

    made this tool output valid shellcode, read in from input file, and print length

--- Assignment-1/test.c
+++ Assignment-1/test.c
@@ -1,21 +1,70 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
-//00000000  31C0              xor eax,eax
-//00000002  40                inc eax
-//00000003  89C3              mov ebx,eax
-//00000005  83C363            add ebx,byte +0x63
-//00000008  CD80              int 0x80
+int main (int argc, char *argv[]) {
+  int i;
+  int fd;
+  int len;
+  unsigned char *shellcode;
+  struct stat s;
+  
 
-char shellcode[] = \
-"\x31\xc0"
-"\x40"
-"\x89\xc3"
-"\x83\xc3\x63"
-"\xcd\x80"
-;
+  if (!argv[1]) {
+    fprintf (stderr, "usage: %s <file>\n", argv[0]);
+    exit (EXIT_FAILURE);
+  }
 
-int main () {
-  printf("Length: %d\n", sizeof(shellcode) - 1);
-  int (*run)() = (int(*)())shellcode;
-  run();
+  printf ("Testing shellcode contained in %s\n\n", argv[0]);
+
+  if (stat(argv[1], &s) == -1) {
+    perror("stat");
+    exit (EXIT_FAILURE);
+  }
+
+  fd = open (argv[1], O_RDONLY);
+  if (fd == -1) {
+    perror ("open");
+    exit (EXIT_FAILURE);
+  }
+
+  shellcode = malloc (s.st_size);
+
+  if (read (fd, shellcode, s.st_size) != s.st_size) {
+    fprintf (stderr, "Unable to read %ld bytes from %s\n", s.st_size, argv[1]);
+    exit (EXIT_FAILURE);
+  }
+
+  close (fd);
+
+  for (i = 0; i < s.st_size; i++) {
+    if (shellcode[i] == 0x00) {
+      printf ("SHELLCODE CONTAINS NULL BYTES!\n\n");
+      break;
+    }
+  }
+
+  /* display shellcode in C format */
+  printf ("char shellcode[] = \"");
+
+  for (i = 0; i < s.st_size; i++) {
+    if ((i % 10 == 0) && (i != 0)) {
+      printf ("\"\n");
+      printf ("                   \"");
+    }
+
+    printf ("\\x%.2x", shellcode[i]);
+  }
+
+  printf ("\";\n\n");
+
+  printf ("Length: %ld\n\n", s.st_size);
+
+  printf ("Executing shellcode..\n");
+  int (*run) () = (int(*)())shellcode;
+  run ();
 }
