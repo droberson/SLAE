e5991deb2bc37de76e03e4dc2688eb7c162d4f29
Daniel Roberson <daniel@planethacker.net>
2017-04-27 07:07:40 -0700

    cleaned up comments

--- Assignment-3/egghunter.asm
+++ Assignment-3/egghunter.asm
@@ -1,12 +1,15 @@
 ; Egg Hunter Shellcode Linux x86
 ; by Daniel Roberson -- SLAE 877
 ;
-;
 
 
 BITS 32
 
-mov ebx, 0x424a424a             ; EGG: inc edx; dec edx; inc edx; dec edx
+; If the egg is changed here, it must be changed in the shellcode as well
+mov ebx, 0x424a424a             ; EGG: inc edx
+                                ;      dec edx
+                                ;      inc edx
+                                ;      dec edx
 loop:
 	inc eax
 	cmp DWORD [eax], ebx
