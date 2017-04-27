72e873a6279753a6e6c16a339c7e856cd1fca57e
Daniel Roberson <daniel@planethacker.net>
2017-04-26 20:45:33 -0700

    working prototype

--- Assignment-3/egghunter.c
+++ Assignment-3/egghunter.c
@@ -1,3 +1,24 @@
-int main() {
-}
+#include <stdio.h>
+#include <string.h>
 
+
+char egghunter[] = "\x99\xba\x42\x4a\x42\x4a\x40\x39\x10\x75"
+                   "\xfb\xff\xe0";
+
+
+/* write "hi there\n" to stdout */
+char shellcode[] = "\x42\x4a\x42\x4a"
+                   /* egg ^^^^^ */ "\x31\xc0\x31\xdb\x31\xc9"
+                   "\x31\xd2\x83\xc0\x04\x43\x6a\x0a\x68\x68"
+                   "\x65\x72\x65\x68\x68\x69\x20\x74\x89\xe1"
+                   "\x83\xc2\x09\xcd\x80\x31\xc0\x40\x4b\xcd"
+                   "\x80";
+
+
+void main() {
+  printf ("Egg hunter length: %d\n", strlen (egghunter));
+  printf ("Shellcode length: %d\n", strlen (shellcode));
+
+  int (*run) () = (int(*)())egghunter;
+  run();
+}
